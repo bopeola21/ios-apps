f3f20864ef9ade78729bb0511683824bde75da88
bopeola21 <bopeola@Jides-MacBook-Air.local>
2015-01-08 09:25:35 -0500

    GetHubFriends

---
+++ GitHubFriends/GitHubFriends/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubFriends
+//
+//  Created by Jide Opeola on 1/8/15.
+//  Copyright (c) 2015 Jide Opeola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
